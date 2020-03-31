a85b452988b04aef8ac6784600b56b96f4275fcc
jarokaz <jarokaz@no-reply.com>
2020-03-31 20:08:58 +0000

    Updates

---
+++ workshops/data-analysis-validation/lab-01-tfdv-structured-data/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+
+setup(
+    name='tfdv',
+    description='TFDV Runtime.',
+    version='0.1',
+    install_requires=[
+      'tensorflow_data_validation==0.21.5'
+    ]
+)
