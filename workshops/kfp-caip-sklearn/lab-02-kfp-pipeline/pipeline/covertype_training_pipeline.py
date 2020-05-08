b4fd820347727268b204d44556919e9dd929857a
Benoit Dherin <dherin@google.com>
2020-05-08 14:18:33 +0200

    now lab-02.ipynb loads the KF pipeline for exercises + pep 8

--- workshops/kfp-caip-sklearn/lab-02-kfp-pipeline/pipeline/covertype_training_pipeline.py
+++ workshops/kfp-caip-sklearn/lab-02-kfp-pipeline/pipeline/covertype_training_pipeline.py
@@ -70,19 +70,19 @@ HYPERTUNE_SETTINGS = """
 
 # Helper functions
 def generate_sampling_query(source_table_name, num_lots, lots):
-  """Prepares the data sampling query."""
+    """Prepares the data sampling query."""
 
-  sampling_query_template = """
-       SELECT *
-       FROM 
-           `{{ source_table }}` AS cover
-       WHERE 
-       MOD(ABS(FARM_FINGERPRINT(TO_JSON_STRING(cover))), {{ num_lots }}) IN ({{ lots }})
-       """
-  query = Template(sampling_query_template).render(
-      source_table=source_table_name, num_lots=num_lots, lots=str(lots)[1:-1])
+    sampling_query_template = """
+         SELECT *
+         FROM 
+             `{{ source_table }}` AS cover
+         WHERE 
+         MOD(ABS(FARM_FINGERPRINT(TO_JSON_STRING(cover))), {{ num_lots }}) IN ({{ lots }})
+         """
+    query = Template(sampling_query_template).render(
+        source_table=source_table_name, num_lots=num_lots, lots=str(lots)[1:-1])
 
-  return query
+    return query
 
 
 # Create component factories
@@ -113,101 +113,101 @@ def covertype_train(project_id,
                     replace_existing_version,
                     hypertune_settings=HYPERTUNE_SETTINGS,
                     dataset_location='US'):
-  """Orchestrates training and deployment of an sklearn model."""
+    """Orchestrates training and deployment of an sklearn model."""
 
-  # Create the training split
-  query = generate_sampling_query(
-      source_table_name=source_table_name, num_lots=10, lots=[1, 2, 3, 4])
+    # Create the training split
+    query = generate_sampling_query(
+        source_table_name=source_table_name, num_lots=10, lots=[1, 2, 3, 4])
 
-  training_file_path = '{}/{}'.format(gcs_root, TRAINING_FILE_PATH)
+    training_file_path = '{}/{}'.format(gcs_root, TRAINING_FILE_PATH)
 
-  create_training_split = bigquery_query_op(
-      query=query,
-      project_id=project_id,
-      dataset_id=dataset_id,
-      table_id='',
-      output_gcs_path=training_file_path,
-      dataset_location=dataset_location)
+    create_training_split = bigquery_query_op(
+        query=query,
+        project_id=project_id,
+        dataset_id=dataset_id,
+        table_id='',
+        output_gcs_path=training_file_path,
+        dataset_location=dataset_location)
 
-  # Create the validation split
-  query = generate_sampling_query(
-      source_table_name=source_table_name, num_lots=10, lots=[8])
+    # Create the validation split
+    query = generate_sampling_query(
+        source_table_name=source_table_name, num_lots=10, lots=[8])
 
-  validation_file_path = '{}/{}'.format(gcs_root, VALIDATION_FILE_PATH)
+    validation_file_path = '{}/{}'.format(gcs_root, VALIDATION_FILE_PATH)
 
-  create_validation_split = bigquery_query_op(
-      query=query,
-      project_id=project_id,
-      dataset_id=dataset_id,
-      table_id='',
-      output_gcs_path=validation_file_path,
-      dataset_location=dataset_location)
+    create_validation_split = bigquery_query_op(
+        query=query,
+        project_id=project_id,
+        dataset_id=dataset_id,
+        table_id='',
+        output_gcs_path=validation_file_path,
+        dataset_location=dataset_location)
 
-  # Create the testing split
-  query = generate_sampling_query(
-      source_table_name=source_table_name, num_lots=10, lots=[9])
+    # Create the testing split
+    query = generate_sampling_query(
+        source_table_name=source_table_name, num_lots=10, lots=[9])
 
-  testing_file_path = '{}/{}'.format(gcs_root, TESTING_FILE_PATH)
+    testing_file_path = '{}/{}'.format(gcs_root, TESTING_FILE_PATH)
 
-  create_testing_split = bigquery_query_op(
-      query=query,
-      project_id=project_id,
-      dataset_id=dataset_id,
-      table_id='',
-      output_gcs_path=testing_file_path,
-      dataset_location=dataset_location)
+    create_testing_split = bigquery_query_op(
+        query=query,
+        project_id=project_id,
+        dataset_id=dataset_id,
+        table_id='',
+        output_gcs_path=testing_file_path,
+        dataset_location=dataset_location)
 
-  # Tune hyperparameters
-  tune_args = [
-      '--training_dataset_path',
-      create_training_split.outputs['output_gcs_path'],
-      '--validation_dataset_path',
-      create_validation_split.outputs['output_gcs_path'], '--hptune', 'True'
-  ]
+    # Tune hyperparameters
+    tune_args = [
+        '--training_dataset_path',
+        create_training_split.outputs['output_gcs_path'],
+        '--validation_dataset_path',
+        create_validation_split.outputs['output_gcs_path'], '--hptune', 'True'
+    ]
 
-  job_dir = '{}/{}/{}'.format(gcs_root, 'jobdir/hypertune',
-                              kfp.dsl.RUN_ID_PLACEHOLDER)
+    job_dir = '{}/{}/{}'.format(gcs_root, 'jobdir/hypertune',
+                                kfp.dsl.RUN_ID_PLACEHOLDER)
 
-  hypertune = mlengine_train_op(
-      project_id=project_id,
-      region=region,
-      master_image_uri=TRAINER_IMAGE,
-      job_dir=job_dir,
-      args=tune_args,
-      training_input=hypertune_settings)
+    hypertune = mlengine_train_op(
+        project_id=project_id,
+        region=region,
+        master_image_uri=TRAINER_IMAGE,
+        job_dir=job_dir,
+        args=tune_args,
+        training_input=hypertune_settings)
 
-  # Retrieve the best trial
-  get_best_trial = retrieve_best_run_op(project_id, hypertune.outputs['job_id'])
+    # Retrieve the best trial
+    get_best_trial = retrieve_best_run_op(
+            project_id, hypertune.outputs['job_id'])
 
-  # Train the model on a combined training and validation datasets
-  job_dir = '{}/{}/{}'.format(gcs_root, 'jobdir', kfp.dsl.RUN_ID_PLACEHOLDER)
+    # Train the model on a combined training and validation datasets
+    job_dir = '{}/{}/{}'.format(gcs_root, 'jobdir', kfp.dsl.RUN_ID_PLACEHOLDER)
 
-  train_args = [
-      '--training_dataset_path',
-      create_training_split.outputs['output_gcs_path'],
-      '--validation_dataset_path',
-      create_validation_split.outputs['output_gcs_path'], '--alpha',
-      get_best_trial.outputs['alpha'], '--max_iter',
-      get_best_trial.outputs['max_iter'], '--hptune', 'False'
-  ]
+    train_args = [
+        '--training_dataset_path',
+        create_training_split.outputs['output_gcs_path'],
+        '--validation_dataset_path',
+        create_validation_split.outputs['output_gcs_path'], '--alpha',
+        get_best_trial.outputs['alpha'], '--max_iter',
+        get_best_trial.outputs['max_iter'], '--hptune', 'False'
+    ]
 
-  train_model = mlengine_train_op(
-      project_id=project_id,
-      region=region,
-      master_image_uri=TRAINER_IMAGE,
-      job_dir=job_dir,
-      args=train_args)
+    train_model = mlengine_train_op(
+        project_id=project_id,
+        region=region,
+        master_image_uri=TRAINER_IMAGE,
+        job_dir=job_dir,
+        args=train_args)
 
-  # Evaluate the model on the testing split
-  eval_model = evaluate_model_op(
-      dataset_path=str(create_testing_split.outputs['output_gcs_path']),
-      model_path=str(train_model.outputs['job_dir']),
-      metric_name=evaluation_metric_name)
+    # Evaluate the model on the testing split
+    eval_model = evaluate_model_op(
+        dataset_path=str(create_testing_split.outputs['output_gcs_path']),
+        model_path=str(train_model.outputs['job_dir']),
+        metric_name=evaluation_metric_name)
 
-  # Deploy the model if the primary metric is better than threshold
-  with kfp.dsl.Condition(
-      eval_model.outputs['metric_value'] > evaluation_metric_threshold):
-    deploy_model = mlengine_deploy_op(
+    # Deploy the model if the primary metric is better than threshold
+    with kfp.dsl.Condition(eval_model.outputs['metric_value'] > evaluation_metric_threshold):
+        deploy_model = mlengine_deploy_op(
         model_uri=train_model.outputs['job_dir'],
         project_id=project_id,
         model_id=model_id,
@@ -215,10 +215,9 @@ def covertype_train(project_id,
         runtime_version=RUNTIME_VERSION,
         python_version=PYTHON_VERSION,
         replace_existing_version=replace_existing_version)
-  
-  # Configure the pipeline to run using the service account defined
-  # in the user-gcp-sa k8s secret
-  if USE_KFP_SA == 'True':
-    kfp.dsl.get_pipeline_conf().add_op_transformer(use_gcp_secret('user-gcp-sa'))
 
-
+    # Configure the pipeline to run using the service account defined
+    # in the user-gcp-sa k8s secret
+    if USE_KFP_SA == 'True':
+        kfp.dsl.get_pipeline_conf().add_op_transformer(
+              use_gcp_secret('user-gcp-sa'))
