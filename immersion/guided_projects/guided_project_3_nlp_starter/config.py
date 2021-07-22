373bc27272920365c183b8e5eb642e090c032913
Benoit Dherin <dherin@google.com>
2021-07-22 20:44:37 +0000

    immersion/guided_project_starter: model.py refactor but still not training

---
+++ immersion/guided_projects/guided_project_3_nlp_starter/config.py
@@ -0,0 +1,14 @@
+
+FEATURE_KEY = 'title'
+LABEL_KEY = 'source'
+N_CLASSES = 3
+HUB_URL = 'https://tfhub.dev/google/tf2-preview/gnews-swivel-20dim-with-oov/1'
+HUB_DIM = 20
+N_NEURONS = 16
+TRAIN_BATCH_SIZE = 100
+EVAL_BATCH_SIZE = 10
+MODEL_NAME = 'tfx_title_classifier'
+
+
+def transformed_name(key):
+    return key + '_xf'
--- immersion/guided_projects/guided_project_3_nlp_starter/preprocessing.py
+++ immersion/guided_projects/guided_project_3_nlp_starter/preprocessing.py
@@ -1,14 +1,13 @@
 
 import tensorflow as tf
-import tensorflow_model_analysis as tfma
 import tensorflow_transform as tft
-from tensorflow_transform.tf_metadata import schema_utils
 
-
-FEATURE_KEY = 'title'
-LABEL_KEY = 'source'
-N_CLASSES = 3
-
+from config import (
+    LABEL_KEY,
+    N_CLASSES,
+    FEATURE_KEY,
+    transformed_name
+)
 
 def _fill_in_missing(x):
     default_value = '' if x.dtype == tf.string else 0
@@ -17,10 +16,6 @@ def _fill_in_missing(x):
             tf.SparseTensor(x.indices, x.values, [x.dense_shape[0], 1]),
             default_value),
         axis=1)
-
-
-def transformed_name(key):
-    return key + '_xf'
 
 
 def process_labels(raw_labels):
@@ -34,7 +29,7 @@ def preprocessing_fn(inputs):
     features = _fill_in_missing(inputs[FEATURE_KEY])
     raw_labels =  _fill_in_missing(inputs[LABEL_KEY])
     labels = process_labels(raw_labels)
-    
+
     return {
         transformed_name(FEATURE_KEY): features,
         transformed_name(LABEL_KEY): labels
